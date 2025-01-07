35dddc5b2c8945ed85887076b65b1588289f45bc
sanauwar <sanauwarnadeem123@gmail.com>
2025-01-08 03:08:14 +0530

    Directory Structure & Update Read me

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # node-api
+
 This repository contains node API with docker container
+
+# If Error(WSL)
+
+Step-1 dockerd &
+
+# Run Docker
+
+Step-2 sudo docker-compose up
--- api/index.js
+++ api/index.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const router = express.Router();
-//const handlers = require('../middleware/handlers')
 const users = require("./users");
-
-//router.all('*', handlers.handle);
 
 router.use("/users", users);
 
